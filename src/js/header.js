cdd9fe0fd1db9fb2fba0439d7b0292f544eadaf4
Daniil Mazurov <rentqaz15@gmail.com>
2024-04-03 19:19:03 +0300

    Удалил старую версию проекта

--- src/js/header.js
+++
@@ -1,6 +0,0 @@
-const openMenu = document.getElementById("openMenu");
-const menuMobile = document.getElementById("menuMobile");
-openMenu.onclick = () => {
-  menuMobile.classList.toggle("menu");
-  openMenu.classList.toggle("menu");
-};
